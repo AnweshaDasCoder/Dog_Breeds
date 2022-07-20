e5756b73e8ccafad9312675d798efd1022b311cf
AnweshaDasCoder <95891366+AnweshaDasCoder@users.noreply.github.com>
2022-07-20 13:17:00 -0400

    Add files via upload
    New version of this website that can detect different types of dogs.

--- main.js
+++ main.js
@@ -14,13 +14,31 @@ function snapshot()
     Webcam.snap(function(data_uri) {
         document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
     });
+
+  }
+
+    //<script>src='https://unpkg.com/ml5@latest/dist/ml5.min.js'</script> 
+ 
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7qw7d1YKGW/model.json',modelLoaded);
+ 
+function modelLoaded() {
+  console.log('Model Loaded! ml5 version:', ml5.version);
 }
 
-console.log('ml5 version:', ml5.version);
-
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7qw7d1YKGW/model.json',modelLoaded);
-
- 
-  function modelLoaded() {
-    console.log('Model Loaded!');
+function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
   }
+
+function gotResult(error, results) {
+ 
+  if (error) {
+    console.error(error);
+  } else {
+   
+    console.log(results);
+     document.getElementById("name").innerHTML ='The type of dog is - ' + results[0].label;
+    document.getElementById("accuracy").innerHTML = 'The accuracy of the dog is - ' + results[0].confidence.toFixed(3) + "%";
+  }
+}
